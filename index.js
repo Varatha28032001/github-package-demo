0c0819a74fb5acf5eca26fe41198bfd879c6583b
Varatha28032001 <135800322+Varatha28032001@users.noreply.github.com>
2024-02-25 00:32:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
-function greet(name) {
-    return Hello, ${name}!;
-}
 
-module.exports = greet;
+// Define a variable
+const name = "World";
+
+// Use template literals to interpolate the variable within a string
+console.log(`Hello, ${name}!`);
